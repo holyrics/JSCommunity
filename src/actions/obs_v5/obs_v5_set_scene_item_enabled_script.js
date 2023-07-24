a99b6a21fa002188f1ee60da4e22cb384e606321
Holyrics <43863376+holyrics@users.noreply.github.com>
2023-07-24 14:40:38 -0300

    Delete src/actions directory

--- src/actions/obs_v5/obs_v5_set_scene_item_enabled_script.js
+++
@@ -1,5 +0,0 @@
-var p1 = obj.input.receiver_id;
-var p2 = obj.input.scene_name;
-var p3 = obj.input.scene_item_name;
-var enabled = jsc.obs_v5.getSceneItemEnabled(p1, p2, p3);
-jsc.obs_v5.setSceneItemEnabled(p1, p2, p3, !enabled);
