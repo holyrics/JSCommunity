2d6827264162000bf278a0268a320fcf92516801
Holyrics <43863376+holyrics@users.noreply.github.com>
2023-08-05 10:29:36 -0300

    add function fixBibleReferences

--- src/includes/utils.js
+++ src/includes/utils.js
@@ -1,5 +1,26 @@
 utils = {
-    example: function() {
-      return h.log('jsc.utils.example OK');
+    example: function () {
+        return h.log('jsc.utils.example OK');
+    },
+    //
+    fixBibleReferences: function (references) {
+        var booksAliases = jsc.data.booksAliases;
+        var lang = jslib.getLanguage();
+        if (booksAliases[lang] === undefined) {
+            return references;
+        }
+
+        for (var book in booksAliases[lang]) {
+            for (var i in booksAliases[lang][book]) {
+                var alias = booksAliases[lang][book][i];
+                // regex group 1: line start or space or punctuation marks
+                // regex group 2: alternative alias itself
+                // regex group 3 space or dot or digits
+                var regex = '(^|\\s|[,;])(' + alias + ')(\\s|\\.|\\d)(?i)';
+                // leave group 1 and 3, but replace 2nd group with canonical name.
+                references = references.replaceAll(regex, '\u00241' + book + '\u00243');
+            }
+        }
+        return references;
     }
 };
