95e6f0833ad11e036dccbab616e1d758ea0c14c3
Holyrics <43863376+holyrics@users.noreply.github.com>
2023-08-05 10:17:46 -0300

    Update lumikit.js

--- src/includes/lumikit.js
+++ src/includes/lumikit.js
@@ -13,8 +13,8 @@ lumikit = {
         //Valor aceito pelo lumikit vai de 0 a 255
         //Porém corresponde relativamente ao BPM de 30 a 200
         //A fórmula abaixo ajusta o BPM para o valor relativo correto
-        var compatibleBPM = Math.round((bpm - 30) / 170 * 255);
-        var data = 'mDMX:UF90:' + parseInt(compatibleBPM).toFixed(0);
+        var relativeBPM = Math.round((bpm - 30) / 170 * 255);
+        var data = 'mDMX:UF90:' + parseInt(relativeBPM).toFixed(0);
         jsc.lumikit.requestUDP(receiverID, data);
     }
 };
