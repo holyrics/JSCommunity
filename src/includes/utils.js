70b3bf597dc8ced681a048b01767574f7453bef6
prcris <cristiano.expede@gmail.com>
2023-08-16 12:18:35 -0300

    Ajustes para a função timeline e ajustes de tradução
    Implementado o objeto timeline com as respectivas funções em um arquivo separado na pasta util e padronizada as traduções. Também adicionados idiomas espanhol e italiano às traduções, bem como complementada as traduções faltantes em russo.

--- src/includes/utils.js
+++ src/includes/utils.js
@@ -22,35 +22,5 @@ utils = {
             }
         }
         return references;
-    },
-	
-    timeline : {
-
-    //  Function to initialize Event
-	startEventLog: function(eventName) {
-        h.store(eventName, '');
-        h.startTimer(eventName);
-        return 'Timeline ' + eventName + ' ' + jsc.i18n('inicializada' + '.';
-    },
-    
-    //  Function to clean Event List
-	clearEventLog: function(eventName) {
-        h.store(eventName, '');
-        return 'Timeline ' + eventName  + ' ' + jsc.i18n('esvaziada') + '.';
-    },
-
-    // Function to add Event into the timeline
-    addEventLog: function(eventName, text) {
-        var eventText = h.restore(eventName) + h.getTimer(eventName) + ' ' + text + '\n';
-        h.store(eventName, eventText);
-        return jsc.i18n('Momento adicionado') +': ' + eventName + ' ' + h.getTimer(eventName) + ' ' + text;
-    },
-
-    // Function to generate the timeline list
-    generateTimeline: function(eventName) {
-        var timeline = h.restore(eventName)
-        return timeline;
     }
-  }	
-  
 };
