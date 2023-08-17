99b7ef65341d32805e96df3a18b196d0fa09241f
prcris <cristiano.expede@gmail.com>
2023-08-17 08:32:29 -0300

    Update lumikit.js
    Melhoria na forma de chamar a função requestUDP, separando os parâmetros do comando e do valor a ser atribuído ao comando, mas mantendo a compatibilidade com o modelo anterior.
    
    Ajustado a setBPM para o novo modelo
    
    Criadas as funções:
    setBPM
    selectFixtureGroup
    selectFixture
    setDimmer
    setStrobo
    setR
    setG
    setB
    setW
    setA
    setU
    setRGBWAU

--- src/includes/lumikit.js
+++ src/includes/lumikit.js
@@ -1,20 +1,62 @@
-lumikit = {
-    requestUDP: function (receiverID, data) {
-        h.apiRequest(receiverID, {
-            headers: {
-                type: 'UDP'
-            },
-            port: 22688,
-            data: data
-        });
-    },
-    //
-    setBPM: function (receiverID, bpm) {
-        //Valor aceito pelo lumikit vai de 0 a 255
-        //Porém corresponde relativamente ao BPM de 30 a 200
-        //A fórmula abaixo ajusta o BPM para o valor relativo correto
-        var relativeBPM = Math.round((bpm - 30) / 170 * 255);
-        var data = 'mDMX:UF90:' + parseInt(relativeBPM).toFixed(0);
-        jsc.lumikit.requestUDP(receiverID, data);
+var jsc = {
+    lumikit: {
+        requestUDP: function (receiverID, data1, data2) {
+            var data = (data1.indexOf('mDMX:') === 0) ? data1 : 'mDMX:' + data1;
+            
+            if (data2 !== undefined) {
+                data += ':' + data2;
+            }
+            Log('Log',data);
+            h.apiRequest(receiverID, {
+                headers: {
+                    type: 'UDP'
+                },
+                port: 22688,
+                data: data
+            });
+        },
+        setBPM: function (receiverID, bpm) {
+            var relativeBPM = Math.round((bpm - 30) / 170 * 255);
+            jsc.lumikit.requestUDP(receiverID, 'UF90', parseInt(relativeBPM).toFixed(0));
+        },
+        selectFixtureGroup: function (receiverID, value) {
+            var relative_Value = Math.round((value-1) / 127 * 255);
+            jsc.lumikit.requestUDP(receiverID, 'MS02', parseInt(relative_Value).toFixed(0));
+        },
+        selectFixture: function (receiverID, value) {
+            var relative_Value = Math.round(value / 127 * 255);
+            jsc.lumikit.requestUDP(receiverID, 'MS03', parseInt(relative_Value).toFixed(0));
+        },
+        setDimmer: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF07', value);
+        },
+        setStrobo: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF31', value);
+        setR: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF32', value);
+        },
+        setG: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF33', value);
+        },
+        setB: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF34', value);
+        },
+        setW: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF35', value);
+        },
+        setA: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF36', value);
+        },
+        setU: function (receiverID, value) {
+            jsc.lumikit.requestUDP(receiverID, 'XF37', value);
+        },
+        setRGBWAU: function (receiverID, R, G, B, W, A, U) {
+            jsc.lumikit.setR(receiverID, R);
+            jsc.lumikit.setG(receiverID, G);
+            jsc.lumikit.setB(receiverID, B);
+            jsc.lumikit.setW(receiverID, W);
+            jsc.lumikit.setA(receiverID, A);
+            jsc.lumikit.setU(receiverID, U);
+        }
     }
 };
