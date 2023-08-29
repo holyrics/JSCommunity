6fb0854e7a5ea964afeb03212770fd1c23c0c1bb
prcris <cristiano.expede@gmail.com>
2023-08-29 16:34:47 -0300

    Gatilho para luz Lumikit aleatória
    implementa um gatilho que possibilita sortear cenas de luzes a cada troca de slide.

---
+++ src/actions/lumikit/random_scene_functions.js
@@ -0,0 +1,14 @@
+function desvendaLuz(marcador) {
+var slide_type = obj.slide_type+obj.slide_description;
+  
+  return (slide_type.indexOf(marcador) > -1) && (obj.bpm > 0);
+}
+
+function checaTag(obj,marcador) {
+  return obj.tag.indexOf(marcador) > -1;
+}
+
+function sorteiaLuz(tipo) {
+  //sorteia uma cena de luz e grava para não repetir no próximo sorteio
+  return tipo[jslib.random(0,tipo.length-1,'luzAleatoria')];
+}
