2f647c6fb22a4e51e87608bb3052bf4ddf6f167a
prcris <cristiano.expede@gmail.com>
2024-04-26 16:41:26 -0300

    Suavidade de volume
    ajuste na suavidade do volume

--- src/actions/x32/set_channel_volume_script.js
+++ src/actions/x32/set_channel_volume_script.js
@@ -1,14 +1,17 @@
-jsc.x32.setSmoothChannelVolume(
-        obj.input.receiver_id,
-        obj.input.channel,
-        obj.input.volume / 100,
-        0.001 * obj.input.speed
-);
+function scriptAction(obj) {
 
+var range = h.getGlobal("jsc.x32.smooth_volume_range", 8);
+range = jsc.utils.range(range, 1, 30);
+var speed = obj.input.speed;
+speed = (range*(Math.exp((speed)/10)-1))/(Math.E-1);
+jsc.x32.setSmoothChannelVolume(obj.input.receiver_id, 
+                          obj.input.channel,
+		        obj.input.volume / 100,
+		        0.001 * speed);
 if (obj.input.unmute) {
-    jsc.x32.setChannelMute(
-            obj.input.receiver_id,
-            obj.input.channel,
-            false
-    );
+    jsc.x32.setChannelMute(obj.input.receiver_id,
+                           obj.input.channel,
+			false);
+     }
+                          
 }
