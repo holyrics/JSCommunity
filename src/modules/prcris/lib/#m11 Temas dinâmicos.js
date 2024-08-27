3bf84376caab844490c20a15a8ac19b43bf74f22
prcris <cristiano.expede@gmail.com>
2024-08-27 18:08:10 -0300

    módulos revisados

---
+++ src/modules/prcris/lib/#m11 Temas dinâmicos.js
@@ -0,0 +1,247 @@
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a22696e666f227d
+// @prcris_#m11_
+function info() {
+    return {
+        id: '@prcris#m11',
+        name: 'Temas dinâmicos',
+        description: '<html>'+
+                     '• Este módulo automatiza a troca de tema e plano de fundo, permitindo ter sempre um fundo diferente'+
+                     ' a cada exibição de uma música. <br>'+
+                     '• Permite especificar um fundo fixo no campo extra <b>tema_fixo</b> em uma música. <br>'+
+                     '• Também funciona para Apresentação Automática <br> '+
+                     '  uso: Crie tags com o nome de cada tema usado nas configurações e adicione os vídeos que deseja usar junto com o tema para que o módulo possa funcionar adequadamente.'+
+                     '  <br><hr>'+
+                     '@ Para mais informações, visite '+"<a href='https://youtube.com/@multimidiaverdadebalneario'>youtube.com/@multimidiaverdadebalneario</a></html>"
+    };
+}
+
+
+
+
+
+
+
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a227472696767657273227d
+//@prcris#m11
+function triggers(module) {
+
+  logState(module.settings.log); // habilita ou desabilita o log de acordo com a configuração  
+
+  var arr = [];
+
+  arr.push({
+    id: "@prcris#m11_temaFixo_title",
+    when: "displaying",
+    item: "any_title_subitem",
+    action: function(obj) {
+
+      var temaFixo = null;
+      
+      if (obj.title == 'Dízimos' || obj.title == 'Ceia') {
+        temaFixo = obj.title;
+        h.hly('SetCurrentBackground', { name: temaFixo });
+      }
+
+      h.setGlobal('temaFixo', temaFixo);
+      h.log('@prcris#m11', 'temaFixo setado para {}', temaFixo);
+    }
+  });
+  
+  
+  
+  arr.push({
+    id: "@prcris#m11_salta_titulo_musica",
+    when: "displaying",
+    item: "any_song_slide",
+    action: function(obj) {
+      if (obj.slide_show_index == -1 && module.settings.skiptitle) {
+        var timeout = h.getGlobal('@prcris#m11_salta_titulo_musica',1000);
+              h.setTimeout(function (obj) {
+                 h.hly('ActionGoToIndex', {index: 1});
+              }, timeout);
+      } 
+    }
+  });
+
+  return arr;
+}
+
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a2273657474696e6773227d
+//@prcris#m11_
+
+function settings() {
+    
+    return [
+        {
+            name: 'Sobre @prcris#m11',
+            description: "<html><hr>Para mais informações acesse <a href='https://youtube.com/@multimidiaverdadebalneario'>youtube.com/@multimidiaverdadebalneario</a></html>",
+            type: 'label'
+        }, {
+            id: 'skiptitle',
+            label: 'Saltar Título Músicas (1s)',
+            type: 'boolean'
+        }, {
+            id: 'title',
+            label: 'Tema Título e artista',
+            type: 'theme'
+           
+        }, {
+            id: 'slow',
+            label: 'Tema Lentas',
+            type: 'theme'
+        }, {
+            id: 'fast',
+            label: 'Tema Rápidas',
+            type: 'theme'
+           
+        }, {
+            id: 'thite',
+            label: 'Tema Dízimos',
+            type: 'theme'
+           
+        }, {
+            id: 'supper',
+            label: 'Tema Ceia',
+            type: 'theme'
+           
+        }, {
+            id: 'pre_service',
+            label: 'Pré Culto',
+            type: 'theme'
+        }, {
+            type: 'separator'
+        }, {
+            id: 'log',
+            label: 'Habilitar log',
+            type: 'boolean',
+            onchange :  function(obj) {
+                logState(obj.input.log); //habilita ou desabilita o log de acordo com a configuração  
+              }
+        }
+    ];
+
+}
+
+
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a22637573746f6d5468656d65227d
+//@prcris#m11_
+function customTheme(module) {
+    function getTheme(obj) {
+       var tema;
+       
+
+            if ( preCulto() ) {                          tema = module.settings.pre_service.name;}
+       else if ( h.getGlobal("temaFixo") == 'Dízimos' ) { tema = module.settings.thite.name;}          
+       else if ( h.getGlobal("temaFixo") == 'Ceia' ) {   tema = module.settings.supper.name;}
+       else if (obj.slide_show_index === -1 ) {          tema = module.settings.title.name;}
+       else if ( obj.bpm > 110 ) {                       tema = module.settings.fast.name;}
+       else {                                            tema = module.settings.slow.name;}
+       
+       if (obj.slide_show_index === 1) {
+           h.log('@prcris#m11', 'Fundo selecionado: {}', tema);
+       }
+       
+       if (!tagExists(tema)) {
+           h.log('', 'Você precisa criar uma tag para o tema: {} poder funcionar no modo aleatório.', tema);
+       }
+        
+       return { intersection :  true,
+                cache_key : tema,
+                tags : [tema],
+                base_theme : tema
+       };
+    }
+               
+    return {
+        song: getTheme,
+        automatic_presentation: getTheme
+    };
+}
+
+
+
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a2273746172745570227d
+function actions(module) {
+    
+    logState(module.settings.log); //habilita ou desabilita o log de acordo com a configuração
+    return null;
+}
+
+
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a2266756e6374696f6e73227d
+//@prcris#m11
+
+function preCulto() {
+  var r = h.hly('GetCurrentSchedule');
+  var s = r.data[0];
+
+  var serviceTime = new Date(s.datetime);
+  var preServiceStart = new Date(serviceTime.getTime() - 60 * 60 * 1000); // Subtrai 1 hora
+
+  var currentTime = new Date();
+
+  // Verifica se a hora atual está entre preServiceStart e serviceTime
+  if (currentTime >= preServiceStart && currentTime < serviceTime) {
+    return true;
+  }
+
+  return false;
+}
+
+function logState(log){ 
+    h.log.setEnabled('@prcris#m11', log);
+}
+
+function compararHora(str) {
+    str = str.split(':');
+    var agora = new Date();
+    var varData = new Date();
+    ['setHours', 'setMinutes', 'setSeconds'].forEach(function(fn, i) {
+        return varData[fn](str[i]);
+    });
+    if (agora == varData) return 0;
+    else return agora > varData ? 1 : -1;
+}
+function tagExists(tag) {
+    var r = h.hly('GetBackgrounds');
+    var tags = [];
+
+    // Percorre os dados para extrair e acumular as tags
+    for (var i = 0; i < r.data.length; i++) {
+        var bg = r.data[i];
+        for (var j = 0; j < bg.tags.length; j++) {
+            var currentTag = bg.tags[j];
+            if (tags.indexOf(currentTag) === -1) { // Verifica se a tag já foi adicionada
+                tags.push(currentTag);
+            }
+        }
+    }
+    // Verifica se a tag existe no array de tags acumuladas
+    return tags.indexOf(tag) !== -1;
+}
+
+
+function showMessage(title,message) {
+var content = [{ type: 'title',label: title },
+              { type: 'separator' },
+              { type: 'title',label: message}];
+h.input(content);
+}
+
+function getThemeData(themeName) {
+    var t = h.hly('GetBackgrounds', {
+        type: 'theme'
+    });
+
+    var themeData = null;
+
+    // Percorre o array `data` para encontrar o tema pelo nome
+    for (var i = 0; i < t.data.length; i++) {
+        if (t.data[i].name === themeName) {
+            themeData = t.data[i];
+            break; // Sai do loop quando o tema é encontrado
+        }
+    }
+
+    return themeData;
+}
---
+++ src/modules/prcris/lib/#m7 Lumikit Automático.js
@@ -0,0 +1,208 @@
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a22696e666f227d
+//@prcris#m7
+function info() {
+    return {
+        id: '@prcris#m7',
+        name: 'Lumikit Automático',
+        description: '<html>'+
+                     '• Sorteia cenas cadastradas como em movimento para versos de Refrão e Instrumental<br>'+
+                     '• Sorteia cenas estáticas para demais versos<br>'+
+                     '• Ajusta o BPM das luzes do Lumikit e do vídeo do Holyrics.<br>'+
+                     '• Usa o campo extra slideBPM da música para estabelecer BPM variável exemplo: <br>'+
+                     '   23,2|30,80,5 que significa:<br>'+
+                     '   no slide 23 divida o bpm por 2, e no 30 ajuste o bpm para 80, 5 segundos após exibir o verso 30.<br><br>'+
+                     '<hr>Para mais informações, acesse '+"<a href='https://youtube.com/@multimidiaverdadebalneario'>youtube.com/@multimidiaverdadebalneario</a></html>"
+    };
+}
+
+
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a2266756e6374696f6e73227d
+// Functions
+//@prcris#m7
+function decodeLight(obj, marker) {
+    var slideType = obj.slide_type + obj.slide_description;  
+    return slideType.indexOf(marker) > -1;
+}
+
+function randomizeLight(type) {
+    // Sorteia uma cena de luz e grava para não repetir no próximo sorteio
+    return type[h.random(0, type.length - 1, 'randomLight')];
+}
+
+function setBpm(BPM, module) {
+    if (BPM > 29) { 
+        h.log('@prcris#m7', 'BPM alterado para: ' + BPM);
+        h.hly('SetBpm', { bpm: BPM });
+        // Ajusta o BPM das luzes
+        jsc.lumikit.setBPM(module.settings.receiverID, BPM);
+    }
+}
+
+function logState(log){ 
+    h.log.setEnabled('@prcris#m7', log);
+}
+
+function setTimeoutBPM(module, bpm, slide, time) {
+    time = time == 0 ? 500 : time;
+    var timeoutDuration = time < 10 ? time * 1000 : time;
+    h.log('@prcris#m7', 'Velocidade será alterada pelo Slide {} para {} depois de {} segundos', [slide, bpm, timeoutDuration / 1000]); 
+    h.setTimeout(function() {
+        h.log('@prcris#m7', '************ Executando timeout');
+        h.log('@prcris#m7', 'Velocidade Alterada pelo Slide {} para {} depois de {} segundos', slide, bpm, timeoutDuration / 1000); 
+        setBpm(bpm, module);
+    }, timeoutDuration, '@prcris#m7_alterBPM'); // Tempo em milissegundos
+}
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a2273657474696e6773227d
+// Settings
+//@prcris#m7
+
+function settings() {
+    return [
+        {
+            name: 'Sobre @prcris#m7',
+            description: "<html><hr>Para mais informações, visite <a href='https://youtube.com/@multimidiaverdadebalneario'>youtube.com/@multimidiaverdadebalneario</a></html>",
+            type: 'label'
+        }, 
+        {
+            id: 'receiverID',
+            name: 'Receptor',
+            description: '',
+            type: 'receiver',
+            receiver: 'lumikit'
+        },
+        {
+            id: 'movingScene',
+            name: 'Cenas em Movimento',
+            description: 'Índice das cenas em movimento, ex: {}',
+            type: 'string',
+            default_value: '9,13,14,15,16'
+        },
+        {
+            id: 'staticScene',
+            name: 'Cenas Estáticas',
+            description: 'Índice das cenas estáticas, ex: {}',
+            type: 'string',
+            default_value: '1,2,3,4,5,6,7'
+        },
+        {
+            id: 'movementRequiresBpm',
+            name: 'Movimento Requer BPM?',
+            description: 'Define se será executado cenas em movimento mesmo se a música não tiver BPM preenchido',
+            type: 'boolean',
+            default_value: true
+        },
+        {
+            id: 'defaultLightScene',
+            name: 'Cena de Luz Padrão',
+            description: 'Índice da cena que você deseja acionar quando a música terminar',
+            type: 'string',
+            default_value: '7'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            id: 'log',
+            label: 'Habilitar log',
+            type: 'boolean',
+            onchange: function(obj) {
+                logState(obj.input.log); // Habilita ou desabilita o log de acordo com a configuração  
+            }
+        }
+    ];
+}
+// __SCRIPT_SEPARATOR__ - info:7b226e616d65223a227472696767657273227d
+// Triggers
+//@prcris#m7
+function triggers(module) {
+    var arr = [];
+    
+    logState(module.settings.log); // Habilita ou desabilita o log de acordo com a configuração  
+
+    if (module.settings.log) {
+        h.openWindow('js_monitor');   
+    }    
+    
+    arr.push({
+        id: 'lumikitChangeScene_@prcris#m7',
+        when: 'displaying',
+        item: 'any_song_slide',
+        action: function(obj) {
+            var receiverID = module.settings.receiverID;
+            var movingScenes = module.settings.movingScene.replace(/\s/g, '').split(',');  // Índice das cenas ritmadas
+            var staticScenes = module.settings.staticScene.replace(/\s/g, '').split(',');    // Índice das cenas estáticas
+            var movementActive = h.getGlobal('@prcris#m7_movementActive', false);
+            var bpm = obj.bpm > 0 || !module.settings.movementRequiresBpm;
+            var code;
+
+            if ((bpm && obj.slide_show_index < 1) ||  
+                decodeLight(obj, '/') ||
+                (decodeLight(obj, '!') && obj.slide_show_index < 4) ||
+                ((decodeLight(obj, '*') || jsc.utils.isInstrumental(obj) || jsc.utils.isChorus(obj)) && bpm && obj.slide_show_index < obj.slide_show_total)
+            ) { 
+                if (movementActive) {
+                    return;
+                }
+                code = randomizeLight(movingScenes);
+            } else {  
+                code = randomizeLight(staticScenes);
+            }
+            h.setGlobal('@prcris#m7_movementActive', movingScenes.indexOf(code) > -1);
+            h.log('@prcris#m7', 'Luz sorteada: {} ', code); 
+            jsc.lumikit.setActiveScene(receiverID, 1, code);
+        }
+    });
+
+    // Verifica se existe um campo extra com marcação do verso, bpm
+    // Usando a seguinte sintaxe: 23,2|30,80 que significa 
+    // no slide 23 divida o bpm por 2, e no 30 ajuste o bpm para 80
+    arr.push({
+        id: "BPM_variable_@prcris#m7",
+        when: "displaying",
+        item: "any_song_slide",
+        action: function(obj) {
+            try {
+                if (obj.slideBPM != null) {
+                    var slides = obj.slideBPM.split('|');
+                    for (var j = 0; j < slides.length; j++) {
+                        var bpmInfo = slides[j].split(',');
+                        var slide = bpmInfo[0];
+                        if (slide == obj.slide_show_index) {
+                             var bpm = bpmInfo[1];
+                             var time = bpmInfo[2] || 0;
+                             h.log('@prcris#m7', 'BPM variável encontrado: {} ', slides[j]);    
+                             if (bpm == '!') { bpm = obj.bpm * 2; }
+                             else if (bpm < 4) { bpm = obj.bpm / bpm; }
+                             setTimeoutBPM(module, parseInt(bpm).toFixed(0), slide, time);
+                        }  
+                    }
+                }
+            } catch (e) { h.log("", 'Erro {}', [e]); }
+        }
+    });
+    
+    arr.push({
+        id: 'lumikitResetScene_@prcris#m7',
+        when: 'closing',
+        item: 'any_song',
+        action: function (obj) {    
+            h.setGlobal('@prcris#m7_movementActive', false);
+            jsc.lumikit.setActiveScene(module.settings.receiverID, 1, module.settings.defaultLightScene);
+        }
+    });
+
+    // Seta a velocidade do ritmo / luzes caso esteja preenchido o BPM na música
+    arr.push({ 
+        id: 'lumikitSetBpm_@prcris#m7',
+        when: 'displaying',
+        item: 'any_song',
+        action: function (obj) {    
+            h.setGlobal('@prcris#m7_movementActive', false);
+            if (obj.bpm > 29) { 
+                setBpm(obj.bpm, module);
+            }
+        }
+    });
+
+    return arr;
+}
