c67497cc9223eebd327671847e49d72bc6f6478f
prcris <cristiano.expede@gmail.com>
2026-09-09 14:11:09 -0300

    feat: restore dedicated WING actions

---
+++ src/actions/wing/up_down_channel_volume_script.js
@@ -0,0 +1,15 @@
+var p1 = obj.input.receiver_id;
+var p2 = obj.input.channel;
+var changeVolume = obj.input.volume * (obj.input.type == "inc" ? 1 : -1) / 100;
+var currentVolume = jsc.wing.getChannelVolume(p1,p2);
+var targetVolume =  currentVolume + changeVolume;
+
+var range = h.getGlobal("jsc.wing.smooth_volume_range", 8);
+range = jsc.utils.range(range, 1, 30);
+var speed = (range * (Math.exp(obj.input.smoothness / 10) - 1)) / (Math.E - 1);
+
+jsc.wing.setSmoothChannelVolume(p1, p2, targetVolume, 0.001 * speed);
+
+if (obj.input.unmute) {
+    jsc.wing.setChannelMute(p1, p2, false);
+}
