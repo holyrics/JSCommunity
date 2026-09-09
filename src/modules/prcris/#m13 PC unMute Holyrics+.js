36f3cd61ed516a4408fc3dc887899bd376343b2f
prcris <cristiano.expede@gmail.com>
2026-09-09 16:37:43 -0300

    style: add icons to PC unMute media actions

--- src/modules/prcris/#m13 PC unMute Holyrics+.js
+++ src/modules/prcris/#m13 PC unMute Holyrics+.js
@@ -1,11 +1,11 @@
 // __SCRIPT_SEPARATOR__ - info:7b226e616d65223a22737461727475705c7530303236696e666f227d
-// v2.16.1 | 2026-09-09
+// v2.16.2 | 2026-09-09
 //#import modules_generic_functions
 //#import wing
 var mID = '@prcris#m13'; 
 var mUID = mID + ''; 
 var pause = false;
-var moduleVersion = '2.16.1';
+var moduleVersion = '2.16.2';
 var currentModule = null;
 
 function formatModuleLog(message, values) {
@@ -876,7 +876,7 @@ function setVolumeGradually(module, receiverID, channel, targetVolume, channelTy
 }
 
 // __SCRIPT_SEPARATOR__ - info:7b226e616d65223a22636f6e74657874416374696f6e73227d
-// v2.16.1 | 2026-09-09
+// v2.16.2 | 2026-09-09
 // Volume individual do canal da mesa por vídeo, áudio ou apresentação automática.
 
 function mediaVolumeStorageKey() {
@@ -1058,7 +1058,8 @@ function getContextVolumeDefault(module, type, item) {
 
 function createSetMediaVolumeContextAction(module, type) {
     return {
-        name: jsc.i18n('Definir volume deste item na mesa') + ' (' + mID + ')',
+        name: jsc.i18n('Definir volume deste item na mesa'),
+        icon: 'volume_up',
         types: [type],
         action: function(evt) {
             var item = evt.item || {};
@@ -1162,7 +1163,8 @@ function createSetMediaVolumeContextAction(module, type) {
 
 function createRemoveMediaVolumeContextAction(type) {
     return {
-        name: jsc.i18n('Remover volume individual deste item') + ' (' + mID + ')',
+        name: jsc.i18n('Remover volume individual deste item'),
+        icon: 'volume_off',
         types: [type],
         action: function(evt) {
             var item = evt.item || {};
